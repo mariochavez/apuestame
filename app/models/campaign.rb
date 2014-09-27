4541ae5ee979abb0230d41bdf51d411250130367
Mario A Chavez <mario.chavez@gmail.com>
2014-09-27 17:51:51 -0500

    Add helper for hstore tags

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,13 @@ class Campaign < ActiveRecord::Base
   belongs_to :organization
 
   validates :name, :organization_id, :amount, :end_date, :description, presence: true
+
+  def tags_flat=(value)
+    tag_values = value.split(',').each{|tag| tag.strip!}
+    self.tags = tag_values
+  end
+
+  def tags_flat
+    self.tags.join(',')
+  end
 end
